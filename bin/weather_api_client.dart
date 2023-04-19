74244fa61a767b31d5d771d309dd4fc85f78b8f9
Anna Kucherova <Akucherova010@gmail.com>
2023-04-20 01:21:04 +0200

    add weather api client class with methods

---
+++ bin/weather_api_client.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'weather.dart';
+
+class WeatherApiClient {
+  static const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  Future<int> getLocationId(String city) async{
+    final locationUrl = Uri.parse('$baseUrl?q=$city&appid=d31cab9ba36058326b481bf2fb2c223e');
+    final locationResponse = await http.get(locationUrl);
+
+    if(locationResponse.statusCode !=200) {
+      throw Exception('Error getting locationId fot city: $city');
+    }
+    final locationJson = jsonDecode(locationResponse.body);
+    return locationJson['id'] as int;
+  }
+
+  Future <Weather> fetchWeather (int locationId) async {
+    final weatherUrl = Uri.parse('$baseUrl?id=$locationId&appid=d31cab9ba36058326b481bf2fb2c223e');
+    final weatherResponse = await http.get(weatherUrl);
+    print(weatherResponse.body);
+    final weatherJson = jsonDecode(weatherResponse.body);
+    return Weather.fromJson(weatherJson);
+  }
+
+  Future<Weather> getWeather (String city) async{
+    final locationId = await getLocationId(city);
+    return fetchWeather(locationId);
+  }
+}
