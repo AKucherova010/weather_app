446791e45f8daf680cbc80f9467f62b12a56e702
Anna Kucherova <Akucherova010@gmail.com>
2023-04-20 17:57:15 +0200

    add gitignore file

--- bin/weather_api_client.dart
+++ bin/weather_api_client.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+import '../auth/keys.dart';
 import 'weather.dart';
 
 class WeatherApiClient {
   static const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
   Future<int> getLocationId(String city) async{
-    final locationUrl = Uri.parse('$baseUrl?q=$city&appid=$key');
+    final locationUrl = Uri.parse('$baseUrl?q=$city&appid=$myOpenWeatherKey');
     final locationResponse = await http.get(locationUrl);
 
     if(locationResponse.statusCode !=200) {
@@ -18,7 +19,7 @@ class WeatherApiClient {
   }
 
   Future <Weather> fetchWeather (int locationId) async {
-    final weatherUrl = Uri.parse('$baseUrl?id=$locationId&appid=$key');
+    final weatherUrl = Uri.parse('$baseUrl?id=$locationId&appid=$myOpenWeatherKey');
     final weatherResponse = await http.get(weatherUrl);
     print(weatherResponse.body);
     final weatherJson = jsonDecode(weatherResponse.body);
